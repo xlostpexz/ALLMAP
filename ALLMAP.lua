f7dd5b3b3b33ec368d61f51d267782e2d9dccf5e
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-07-13 17:25:22 +0700

    Update and rename README.md to ALLMAP.lua

---
+++ ALLMAP.lua
@@ -0,0 +1,17 @@
+if game.PlaceId == 4520749081 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/KingRaid/main/KingRaid.lua", true))()
+elseif game.PlaceId == 9790558424 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/MasterPirate/main/MasterPirate.lua", true))()
+elseif game.PlaceId == 6381829480 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/KingRaid/main/KingRaid.lua", true))()
+elseif game.PlaceId == 4587545091 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/Vietnampiece/main/VietNamPiece.lua", true))()
+elseif game.PlaceId == 4996049426 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/AllStar/main/AllStar.lua", true))()
+elseif game.PlaceId == 6329844902 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/LastPirate/main/LastPirate.lua", true))()
+elseif game.PlaceId == 7261261021 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/RagingSeas/main/Raging%20Seas.lua", true))()
+elseif game.PlaceId == 9645099707 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/VietPiece/main/VietPiece", true))()
+end
