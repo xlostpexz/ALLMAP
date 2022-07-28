00512c6b81764a81710794fe11c931c20e475a99
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-07-28 17:26:25 +0700

    Update ALLMAP.lua

--- ALLMAP.lua
+++ ALLMAP.lua
@@ -14,8 +14,6 @@ elseif game.PlaceId == 6329844902 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/LastPirate/main/LastPirate.lua", true))()
 elseif game.PlaceId == 7261261021 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/RagingSeas/main/Raging%20Seas.lua", true))()
-elseif game.PlaceId == 9645099707 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/VietPiece/main/VietPiece", true))()
 elseif game.PlaceId == 9875254583 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/JuicePirates/main/Juice%20Pirates.lua", true))()
 end
