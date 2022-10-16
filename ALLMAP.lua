d476fda1c1b1bac0801dba399335713138da458b
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-10-16 18:47:03 +0700

    Update ALLMAP.lua

--- ALLMAP.lua
+++ ALLMAP.lua
@@ -1,6 +1,6 @@
         local placeId = game.PlaceId
         if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/killertest/Fps/Loading.lua"))()
+    loadstring(game:HttpGet('https://scripts.luawl.com/12985/BloxFruits.lua'))()
         elseif placeId == 5931540094 and placeId == 6381829480 and placeId == 4520749081 then
-    loadstring(game:HttpGet("hhttps://raw.githubusercontent.com/xlostpexz/kptestz/Fps/Loading.lua"))()
+    loadstring(game:HttpGet('https://scripts.luawl.com/12984/KingLegacy.lua'))()
         end
